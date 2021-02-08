fd6fa0f6e06b92e977f3fc8e2fc8dab8e77d71f6
yirenzhi <yiren_zhi@163.com>
2021-02-08 19:14:39 +0800

    ch08 80%

---
+++ 2.C++Primer/8.TheIO_Library/8.3/8.7.cpp
@@ -0,0 +1,34 @@
+#include"../../7.Classes/7.3/7_26.h"
+#include<fstream>
+
+int main(int argc,char **argv)
+{
+    Sales_data total;
+    cout<<"argc:"<<argc<<endl;
+    if(argc<3)
+    {
+        cout<<"error"<<endl;
+        return 0;
+    }
+    fstream f(argv[1]);
+    ofstream f1(argv[2]);
+    if(read(f,total)){
+        Sales_data trans;
+        while(read(f,trans))
+        {
+            if(trans.isbn()==total.isbn())
+            {
+                total.combine(trans);
+            }
+            else{
+                print(f1,total);
+                total = trans;
+            }
+        }
+        print(cout,total);
+    }
+    else{
+        cerr<<"No data"<<endl;
+    }
+    return 0;
+}
